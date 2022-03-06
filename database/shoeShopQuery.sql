7c999ecc9356f56408434677a72541c5151c7d2d
Zi Hui <46946696+Heng-Zi-Hui@users.noreply.github.com>
2022-03-07 03:35:40 +0800

    updated shoes table in the database, added images for each product

--- database/shoeShopQuery.sql
+++ database/shoeShopQuery.sql
@@ -61,10 +61,11 @@ CREATE TABLE `item` (
   `itemDescription` varchar(512) NOT NULL,	/* Chuck 70 - Hi - Black */
   `brand` varchar(128) NOT NULL,			/* eg. Converse */
   `sex` varchar(128) NOT NULL,				/* unisex/women/men */
-  `category` varchar(128) NOT NULL,			/* eg. converseBlackHighTops.png */
+  `category` varchar(128) NOT NULL,			/* eg. lifestyle/running/sports/slides */
   `price` decimal(6,2) NOT NULL,			/* eg. 79.90 */
   `points` int(6) NOT NULL,					/* eg. 35  [5 points for every 10 dollars??]*/
   `imageFile` varchar(128) NOT NULL, 		/* eg. converseChuck70Black.jpg */
+  `stock` int(6) NOT NULL,					/* eg. 10 */
   PRIMARY KEY (`itemId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -75,7 +76,31 @@ CREATE TABLE `item` (
 
 LOCK TABLES `shoes` WRITE;
 /*!40000 ALTER TABLE `shoes` DISABLE KEYS */;
-/*INSERT INTO `shoes` VALUES (48,'Samsung 32GB BAR (METAL) USB 3.0 Flash Drive (MUF-32BA/AM)','Samsung',18.50,6),(49,'SanDisk Cruzer CZ36 64GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-064G-AFFP','SanDisk',24.00,7),(50,'SanDisk Cruzer Blade 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ50-032G-AFFP','SanDisk',14.50,4),(51,'TOPSELL 5 Pack 16GB USB 2.0 Flash Drive Memory Stick Thumb Drives (5 Mixed Colors: Black Blue Green Red Silver)','TOPSELL',39.00,12),(52,'SanDisk Cruzer CZ36 32GB USB 2.0 Flash Drive, Frustration-Free Packaging- SDCZ36-032G-AFFP','SanDisk',15.00,5),(53,'SanDisk Cruzer Glide CZ60 128GB USB 2.0 Flash Drive- SDCZ60-128G-B35','SanDisk',43.50,13),(54,'Samsung 64GB BAR (METAL) USB 3.0 Flash Drive (MUF-64BA/AM)','Samsung',27.00,8),(55,'KOOTION 5PCS 1GB USB Flash Drives Thumb Drives Memory Stick USB 2.0(5 Colors: Black Blue Green Purple Red)','KOOTION',26.50,8),(56,'WD 4TB Elements Portable External Hard Drive - USB 3.0 - WDBU6Y0040BBK-WESN','Western Digital',165.00,50),(57,'Seagate Expansion 1TB Portable External Hard Drive USB 3.0 (STEA1000400)','Seagate',82.50,25),(58,'Seagate Backup Plus Slim 2TB Portable External Hard Drive USB 3.0, Silver (STDR2000101)','Seagate',105.00,32),(59,'WD 4TB Black My Passport Portable External Hard Drive - USB 3.0 - WDBYFT0040BBK-WESN','Western Digital',165.00,50),(60,'Silicon Power 1TB Rugged Armor A60 Military-grade Shockproof/Water-Resistant USB 3.0 2.5\" External Hard Drive for','Silicon Power',88.50,27),(61,'WD 2TB Silver My Passport Ultra Metal Edition Portable External Hard Drive - USB 3.0 - WDBEZW0020BSL-NESN','Western Digital',120.00,36),(62,'Toshiba Canvio Connect II 1TB Portable Hard Drive, Red (HDTC810XR3A1)','Toshiba',85.00,26),(63,'LaCie Rugged Mini USB 3.0 / USB 2.0 1TB Portable Hard Drive LAC301558','LaCie',135.00,41),(64,'HP Wired USB Keyboard K1500 (Black)','HP',12.00,4),(65,'Logitech MK270 Wireless Keyboard and Mouse Combo','Logitech',29.50,9),(66,'Logitech Wireless Keyboard K360 - Glossy Black','Logitech',30.00,9),(67,'Rii RK100 3 Colors LED Backlit Mechanical Feeling USB Wired Multimedia Keyboard For working or prime gaming','Rii',28.50,9),(68,'Logitech Wireless Wave Combo MK550 - Curved Comfort, Black (920-002555)','Logitech',66.00,20),(69,'Verbatim Slimline Keyboard - Wired with USB Accessibility - Mac & PC Compatible - Black','Verbatim',10.00,3),(70,'Microsoft Natural Ergonomic Keyboard 4000 for Business - Wired','Microsoft',78.00,23),(71,'Apple Magic Keyboard with Numeric Keypad & Apple Magic Trackpad 2 Bundle','Magic Keyboard and Trackpad',372.00,112),(72,'HP 15.6\" HD WLED Backlit Display Laptop, AMD A6-7310 Quad-Core APU 2GHz, 4GB RAM, 500GB HDD WiFi, DVD+/-RW,','HP',397.50,119),(73,'Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB','Acer',525.00,158),(74,'2017 Newest Lenovo Premium Built High Performance 15.6 inch HD Laptop Intel Pentium Dual-Core Processor 6GB RAM 1T HDD DVD','Lenovo',452.50,136),(75,'Lenovo 110s Premium Built High Performance 11.6 inch HD Laptop pc Intel Celeron Dual-Core Processor 2GB RAM 32G eMMC','Lenovo',236.50,71),(76,'ASUS L402SA Portable Lightweight Laptop PC, Intel Dual Core Processor, 4GB RAM, 32GB Flash Storage with Windows 10 with 1','Asus',283.50,85),(77,'HP 17.3 inch HD+ Flagship High Performance Laptop PC, Intel Core i7-7500U 2.7GHz Dual-Core, 8GB DDR4, 1TB HDD, DVD RW,','HP',828.00,248),(78,'HP Stream Laptop PC 14-ax010nr (Intel Celeron N3060, 4 GB RAM, 32 GB eMMC) with Office 365 Personal for one year','HP',352.00,106),(79,'Lenovo Ideapad 15.6\" HD Premium High Performance Laptop (2017 Newest), AMD A12-9720P Quad core processor 2.7GHz, 8GB','Lenovo',585.00,176),(80,'Acer R240HY bidx 23.8-Inch IPS HDMI DVI VGA (1920 x 1080) Widescreen Monitor','Acer',195.00,59),(81,'LG 32MA68HY-P 32-Inch IPS Monitor with Display Port and HDMI Inputs','LG',300.00,90),(82,'Acer G206HQLbd 19.5-Inch LED Computer Monitor Back-Lit Widescreen Display','Acer',118.00,35),(83,'Asus VS239H-P 23-Inch Full HD 1920x1080 IPS HDMI DVI VGA Back-lit LED Monitor','Asus',178.00,53),(84,'Samsung SF350 Series 19-Inch Slim Design Monitor (S19F350)','Samsung',120.00,36),(85,'HP 23.8-inch FHD IPS Monitor with Tilt/Height Adjustment and Built-in Speakers (VH240a, Black)','HP',195.00,59),(86,'Dell E1916HV VESA Mountable 19\" Screen LED-Lit Monitor','Dell',92.50,28),(87,'Sceptre E248W-1920 24-Inch Screen LED-Lit Monitor','Sceptre',150.00,45);*/
+INSERT INTO `shoes` VALUES ('Stan Smith Shoes - Original - Blue','Adidas','unisex','lifestyle',150.00,75,'stanSmithShoesOriginalBlue.jpg',25),
+						   ('Stan Smith Shoes - Original - Cloud White','Adidas','unisex','lifestyle',150.00,75,'stanSmithShoesOriginalCloudWhite.jpg',5),
+                           ('Stan Smith Shoes - Original - Green','Adidas','unisex','lifestyle',150.00,75,'stanSmithShoesOriginalGreen.jpg',10),
+                           ('Yeezy Slides - Black','Adidas','unisex','slides',120.00,60,'yeezySlidesBlack.jpeg',7),
+                           ('Yeezy Slides - Pure','Adidas','unisex','slides',120.00,60,'yeezySlidesPure.jpeg',5),
+                           ('Ultraboost 22 Shoes - Black','Adidas','men','running',260.00,130,'ultraboost22ShoesBlack.jpg',15),
+                           ('Ultraboost 22 Shoes - Magic Grey','Adidas','men','running',260.00,130,'ultraboost22ShoesMagicGrey.jpg',10),
+                           ('Ultraboost 22 Shoes - Wonder Mauve','Adidas','women','running',260.00,130,'ultraboost22ShoesWonderMauve.jpg',15),
+                           ('Ultraboost 22 Shoes - Turbo','Adidas','women','running',260.00,130,'ultraboost22ShoesTurbo.jpg',7),	
+                           ('Chuck 70 - Hi - Black','Converse','unisex','lifestyle',129.90,64,'chuck70HiBlack.jpeg',20),
+                           ('Chuck 70 - Hi - Parchment','Converse','unisex','lifestyle',129.90,64,'chuck70HiParchment.jpeg',12),
+                           ('Chuck 70 - Hi - Embroidered Floral','Converse','female','lifestyle',95.00,47,'chuck70HiEmbroideredFloral.jpeg',5),
+                           ('Chuck Taylor All Star - Ox - Charcoal','Converse','unisex','lifestyle',75.90,37,'chuckTaylorAllStarOxCharcoal.jpeg',9),
+                           ('Chuck Taylor All Star - Ox - Maroon','Converse','unisex','lifestyle',75.90,37,'chuckTaylorAllStarOxMaroon.jpeg',9),
+                           ('Jordan Zoom Separate PF - Black','Nike','men','sports',179.00,89,'jordanZoomSeparatePFBlack.jpg',11),
+                           ('Nike Air Zoom SuperRep 3 - Black','Nike','men','sports',199.00,99,'nikeAirZoomSuperRep3Black.jpg',14),
+                           ('Kyrie Infinity EP - Multi-Color','Nike','women','sports',219.00,109,'kyrieInfinityEPMultiColor.jpg',10),
+                           ('Nike Free Metcon 4 - Rose','Nike','women','sports',199.00,99,'nikeFreeMetcon4Rose.jpg',12),
+                           ('Mexico 66 SD - Cream','Onitsuka Tiger','unisex','lifestyle',199.00,99,'mexico66SDCream.jpeg',15),
+                           ('Dentigre LS - Birch','Onitsuka Tiger','unisex','sports',229.00,114,'dentigreLSBirch.jpeg',17),
+                           ('Mercedes F1 R-Cat Machina Shoes - White','Puma','unisex','sports',79.00,114,'mercedesF1R-CatMachinaShoesWhite.jpg',20),
+                           ('Popcat 20 Sandals - Black','Puma','men','slides',39.00,19,'popcat20SandalsBlack.jpeg',10),
+                           ('Popcat 20 Sandals - White','Puma','women','slides',39.00,19,'popcat20SandalsWhite.jpeg',10),
+                           ('200 - Blue','New Balance','unisex','slides',49.00,24,'200Blue.jpg',15),
+                           ('Vision Racer - Kumquat','New Balance','unisex','running',209.00,104,'visionRacerKumquat.jpg',25);
 /*!40000 ALTER TABLE `shoes` ENABLE KEYS */;
 UNLOCK TABLES;
 
